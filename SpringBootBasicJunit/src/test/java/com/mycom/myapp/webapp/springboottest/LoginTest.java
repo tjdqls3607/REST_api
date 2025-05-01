2d8212b3982b94bb6e22c8c215f825bb81fc2887
tjdqls3607 <tjdqls3124@gmail.com>
2025-05-01 10:53:58 +0900

    스프링부트테스트 로그인 테스트

--- SpringBootBasicJunit/src/test/java/com/mycom/myapp/webapp/springboottest/LoginTest.java
+++ SpringBootBasicJunit/src/test/java/com/mycom/myapp/webapp/springboottest/LoginTest.java
@@ -2,15 +2,22 @@ package com.mycom.myapp.webapp.springboottest;
 
 
 import com.mycom.myapp.auth.controller.LoginController;
+import com.mycom.myapp.auth.service.LoginService;
 import com.mycom.myapp.user.dto.UserResultDto;
+
+import com.mycom.myapp.user.entity.User;
+import com.mycom.myapp.user.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @SpringBootTest
@@ -33,5 +40,26 @@ public class LoginTest {
                 loginController.login("user1@mycom.com", "password1", httpSession);
         assertEquals("success", userResultDto.getResult());
 
+    }
+
+    // Service
+    @Autowired
+    private LoginService loginService;
+
+    @Test
+    public void testLogin2() {
+        UserResultDto userResultDto = loginService.login("user1@mycom.com", "password1");
+        assertEquals("success", userResultDto.getResult());
+        assertNotNull(userResultDto.getUserDto());
+    }
+
+    // Repositorry
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testLogin3() {
+        Optional<User> optionalUser =  userRepository.findByEmail("user1@mycom.com");
+        assertNotNull(optionalUser.get());
     }
 }
